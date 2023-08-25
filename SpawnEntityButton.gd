2949099e1fe418b0794f7df314b7306674929a09
DragonWarrior12 <james.gravett.01@gmail.com>
2023-08-25 17:49:22 +0100

    remove client spawning

--- SpawnEntityButton.gd
+++ SpawnEntityButton.gd
@@ -3,13 +3,6 @@ extends Button
 @export var entity: PackedScene
 
 func _pressed():
-	if (multiplayer.is_server()):
-		createEntity(($"/root/Countertop/Player" as Node2D).position)
-	else:
-		createEntity.rpc_id(1, ($"/root/Countertop/Player" as Node2D).position)
-
-@rpc("any_peer", "reliable")
-func createEntity(pos):
 	var newEntity = entity.instantiate()
 	$"/root/Countertop/Entities".add_child(newEntity, true)
-	newEntity.rpc("SetPos", pos)
+	newEntity.rpc("RpcSetPos", ($"/root/Countertop/Player" as Node2D).position)
